a83896cbfb1f611408405c9f13324d6d275dd172
adgor <adgor000@gmail.com>
2022-09-17 10:41:22 +0200

    add router

--- apps/react-17/src/App.js
+++ apps/react-17/src/App.js
@@ -1,12 +1,17 @@
 import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Details from "./Details";
 import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
       <h1>Adopt me</h1>
-      <SearchParams />
-    </div>
+      <Routes>
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/" element={<SearchParams />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
