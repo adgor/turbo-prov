1c8edef225e8e8ce9de45c2320ee7843523c0ce6
adgor <adgor000@gmail.com>
2022-09-17 16:29:49 +0200

    add class properties && update comp

--- apps/react-17/src/App.js
+++ apps/react-17/src/App.js
@@ -1,12 +1,14 @@
 import { render } from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Details from "./Details";
 import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <h1>Adopt me</h1>
+      <header>
+        <Link to="/">Adopt me</Link>
+      </header>
       <Routes>
         <Route path="/details/:id" element={<Details />} />
         <Route path="/" element={<SearchParams />} />
--- apps/react-17/src/Details.js
+++ apps/react-17/src/Details.js
@@ -1,5 +1,61 @@
-const Details = () => {
-  return <div>Details</div>;
+import { Component } from "react";
+import { useParams } from "react-router-dom";
+import Carousel from "./Carousel";
+
+class Details extends Component {
+  // constructor(props) {
+  //   super(props);
+
+  //   this.state = { loading: true };
+  // }
+
+  // replace the code above
+  // from class properties
+  state = { loading: true };
+
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.params.id}`
+    );
+    const json = await res.json();
+
+    // if possible call setState Once
+    this.setState({ loading: false, ...json.pets[0] });
+
+    // this.setState({
+    //   loading: false,
+    // });
+
+    // this.setState(json.pets[0]);
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h2>loading ...</h2>;
+    }
+
+    const { animal, breed, city, state, description, name, images } =
+      this.state;
+    return (
+      <div className="details">
+        <Carousel images={images} />
+        <div>
+          <h2>{name}</h2>
+          <h3>
+            {animal} - {breed} - {city}, {state}
+          </h3>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+const WrappedDetails = () => {
+  const params = useParams();
+
+  return <Details params={params} />;
 };
 
-export default Details;
+export default WrappedDetails;
